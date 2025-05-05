e16d0e93254091ac3be407b790fb6dca31d21b66
人間工作 <coolxiaom95@gmail.com>
2025-05-05 21:41:58 +0800

    setup player data if not loaded
    #1

--- src/main/java/top/mrxiaom/sweet/mmorpg/comp/AuraSkillsListener.java
+++ src/main/java/top/mrxiaom/sweet/mmorpg/comp/AuraSkillsListener.java
@@ -1,8 +1,11 @@
 package top.mrxiaom.sweet.mmorpg.comp;
 
 import dev.aurelium.auraskills.api.event.user.UserLoadEvent;
+import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.player.PlayerData;
 import net.Indyuce.mmoitems.api.player.RPGPlayer;
+import net.Indyuce.mmoitems.manager.data.PlayerDataManager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import top.mrxiaom.pluginbase.func.AutoRegister;
@@ -16,13 +19,21 @@ public class AuraSkillsListener extends AbstractModule implements Listener {
         super(plugin);
         registerEvents();
     }
-
     @EventHandler
     public void onUserLoad(UserLoadEvent e) {
-        PlayerData data = PlayerData.get(e.getPlayer().getUniqueId());
+        PlayerData data = getOrSetup(e.getPlayer());
         RPGPlayer rpg = data.getRPG();
         if (rpg instanceof PlayerAuraSkills) {
             ((PlayerAuraSkills) rpg).setUser(e.getUser());
         }
+    }
+
+    private static PlayerData getOrSetup(Player player) {
+        PlayerDataManager manager = MMOItems.plugin.getPlayerDataManager();
+        PlayerData data = manager.getOrNull(player);
+        if (data != null) {
+            return data;
+        }
+        return manager.setup(player);
     }
 }
