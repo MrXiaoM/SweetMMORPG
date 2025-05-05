94e303cf8ce8bd2b178b4ef20ded5dfad932f3b1
人間工作 <coolxiaom95@gmail.com>
2025-05-05 17:19:47 +0800

    allow manager to disable regeneration

--- src/main/java/top/mrxiaom/sweet/mmorpg/comp/EnumManager.java
+++ src/main/java/top/mrxiaom/sweet/mmorpg/comp/EnumManager.java
@@ -1,5 +1,12 @@
 package top.mrxiaom.sweet.mmorpg.comp;
 
 public enum EnumManager {
-    BuiltIn
+    BuiltIn(true, true),
+
+    ;
+    public final boolean regenMana, regenStamina;
+    EnumManager(boolean regenMana, boolean regenStamina) {
+        this.regenMana = regenMana;
+        this.regenStamina = regenStamina;
+    }
 }
--- src/main/java/top/mrxiaom/sweet/mmorpg/func/StatRegenManager.java
+++ src/main/java/top/mrxiaom/sweet/mmorpg/func/StatRegenManager.java
@@ -1,5 +1,6 @@
 package top.mrxiaom.sweet.mmorpg.func;
 
+import net.Indyuce.mmoitems.api.player.RPGPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.scheduler.BukkitTask;
@@ -8,6 +9,7 @@ import top.mrxiaom.sweet.mmorpg.SweetMMORPG;
 import top.mrxiaom.sweet.mmorpg.api.ResourceData;
 import top.mrxiaom.sweet.mmorpg.api.ResourceRegainReason;
 import top.mrxiaom.sweet.mmorpg.api.StatType;
+import top.mrxiaom.sweet.mmorpg.comp.EnumManager;
 
 @AutoRegister
 public class StatRegenManager extends AbstractModule {
@@ -25,10 +27,11 @@ public class StatRegenManager extends AbstractModule {
         }
         regenRate = config.getLong("regen-rate", 10L);
         task = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+            EnumManager manager = plugin.getManagerType();
             for (ResourceData data : plugin.getPlayerDatabase().getCaches()) {
                 if (data.toMythicLib().isOnline()) {
-                    data.giveMana(data.getStat(StatType.MANA_REGENERATION) * regenRate / 20, ResourceRegainReason.REGENERATION);
-                    data.giveStamina(data.getStat(StatType.STAMINA_REGENERATION) * regenRate / 20, ResourceRegainReason.REGENERATION);
+                    if (manager.regenMana) data.giveMana(data.getStat(StatType.MANA_REGENERATION) * regenRate / 20, ResourceRegainReason.REGENERATION);
+                    if (manager.regenStamina) data.giveStamina(data.getStat(StatType.STAMINA_REGENERATION) * regenRate / 20, ResourceRegainReason.REGENERATION);
                 }
             }
         }, 100L, regenRate);
