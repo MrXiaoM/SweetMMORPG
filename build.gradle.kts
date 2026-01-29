b18622f44d9ceb2cd835797bbdb92639a04a1488
MrXiaoM <coolxiaom95@gmail.com>
2026-01-29 14:07:01 +0800

    update 1.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 val base = top.mrxiaom.gradle.LibraryHelper(project)
 
 group = "top.mrxiaom.sweet.mmorpg"
-version = "1.0.3"
+version = "1.0.4"
 val targetJavaVersion = 8
 val pluginBaseModules = base.modules.run { listOf(library) }
 val shadowGroup = "top.mrxiaom.sweet.mmorpg.libs"
